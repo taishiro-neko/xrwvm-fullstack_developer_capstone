7aeeffb4b88ebdc69ac7ae657e307b1b60041404
Cat <catness@gmail.com>
2025-12-10 17:49:59 +0200

    more lint fixes

--- server/database/app.js
+++ server/database/app.js
@@ -34,7 +34,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -91,7 +91,7 @@ app.get('/fetchDealer/:id', async (req, res) => {
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
   const documents = await Reviews.find().sort( { id: -1 } );
-  let new_id = documents[0]['id']+1;
+  let new_id = parseInt(documents[0].id, 10) + 1;
 
   const review = new Reviews({
 		"id": new_id,
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -48,8 +48,6 @@ def logout_request(request):
 
 @csrf_exempt
 def registration(request):
-    #context = {}
-
     # Load JSON data from the request body
     data = json.loads(request.body)
     username = data['userName']
@@ -58,7 +56,6 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    #email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
@@ -142,7 +139,7 @@ def add_review(request):
     if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
-            #response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
         except BaseException:
             return JsonResponse(
